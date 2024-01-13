9d5f4e35e67b317420b73de9b9e1b4e7557dfb91
Russel Smith <russelsmith0125@hotmail.com>
2024-01-13 22:07:33 +0545

    changes in home blade and cards

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -1,23 +1,79 @@
 @extends('customer.layouts.app')
 
 @section('container')
+<style>
+    .img_div img {
+        /* object-fit: cover; */
+        width: 100%;
+        /* height: 800px; */
+    }
 
-<div>
-    <img src="{{ asset('admin_asset/images/small/img-11.jpg') }}" width="100%" height="800px" alt="">
+    .card {
+        cursor: pointer;
+        transition: 0.3s;
+        padding: 10px;
+    }
+
+    .card:hover {
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    }
+</style>
+
+<div class="img_div">
+    <img src="{{ asset('admin_asset/images/small/img-11.jpg') }}" alt="">
 </div>
 
 
-<div class="container mt-0">
-    <div class="row">
-        <div class="col">
+<div class="container mt-5 mb-5">
+    <div class="row border border-primary d-flex align-items-center">
+        <div class="col-sm-6 col-xl-3">
+            <!-- Simple card -->
+            <div class="card">
+                <img class="card-img-top img-fluid" src="{{ asset('admin_asset/images/small/img-11.jpg') }}" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title mb-2">Web Developer</h4>
+                    <p class="card-text">Price</p>
+                </div>
+            </div><!-- end card -->
+        </div><!-- end col -->
+        <div class="col-sm-6 col-xl-3">
+            <!-- Simple card -->
+            <div class="card">
+                <img class="card-img-top img-fluid" src="{{ asset('admin_asset/images/small/img-11.jpg') }}" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title mb-2">Web Developer</h4>
+                    <p class="card-text">Price</p>
+                </div>
+            </div><!-- end card -->
+        </div><!-- end col -->
+        <div class="col-sm-6 col-xl-3">
+            <!-- Simple card -->
+            <div class="card">
+                <img class="card-img-top img-fluid" src="{{ asset('admin_asset/images/small/img-11.jpg') }}" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title mb-2">Web Developer</h4>
+                    <p class="card-text">Price</p>
+                </div>
+            </div><!-- end card -->
+        </div><!-- end col -->
+        <div class="col-sm-6 col-xl-3">
+            <!-- Simple card -->
+            <div class="card">
+                <img class="card-img-top img-fluid" src="{{ asset('admin_asset/images/small/img-11.jpg') }}" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title mb-2">Web Developer</h4>
+                    <p class="card-text">Price</p>
+                </div>
+            </div><!-- end card -->
+        </div><!-- end col -->
+
+        <!-- <h1>This is home</h1>
             <h1>This is home</h1>
             <h1>This is home</h1>
             <h1>This is home</h1>
             <h1>This is home</h1>
-            <h1>This is home</h1>
-            <h1>This is home</h1>
-            <!-- Add more content here to exceed the width -->
-        </div>
+            <h1>This is home</h1> -->
+        <!-- Add more content here to exceed the width -->
     </div>
 </div>
 
