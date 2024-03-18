b97fb1c7661670f2a4b585512597c90ab5d445f8
Russel Smith <russelsmith0125@hotmail.com>
2024-03-18 20:48:54 +0545

    started working on dynamic dashboard page from both front and backend

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
+
 
 class DashboardController extends Controller
 {
@@ -16,6 +21,12 @@ class DashboardController extends Controller
             ],
 
         ];
-        return view('admin.dashboard.dashboard',compact('breadcrumb'));
+        $totalOrders = Order::where('status', '!=', 'cancelled')->count();
+        $totalProducts = Product::count();
+        $totalRevenue=Order::where('status','!=','cancelled')->sum('grand_total');
+        $totalCustomers = Role::where('id', 3)->count();
+
+        $user = Auth::user();
+        return view('admin.dashboard.dashboard', compact('breadcrumb', 'user','totalOrders','totalCustomers','totalProducts','totalRevenue'));
     }
 }
