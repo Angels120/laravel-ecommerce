c8ffab6381ff63c52d44c9c38dda0e6097a7078b
Russel Smith <russelsmith0125@hotmail.com>
2024-03-19 19:10:30 +0545

    dashboard dynamic fix aand starrted working on product revirews from both sides

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Role;
@@ -23,10 +24,25 @@ class DashboardController extends Controller
         ];
         $totalOrders = Order::where('status', '!=', 'cancelled')->count();
         $totalProducts = Product::count();
-        $totalRevenue=Order::where('status','!=','cancelled')->sum('grand_total');
         $totalCustomers = Role::where('id', 3)->count();
-
         $user = Auth::user();
-        return view('admin.dashboard.dashboard', compact('breadcrumb', 'user','totalOrders','totalCustomers','totalProducts','totalRevenue'));
+        $totalRevenue=Order::where('status','!=','cancelled')->sum('grand_total');
+        //This month revenue
+        $startOfMonth=Carbon::now()->startOfMonth()->format('Y-m-d');
+        $currentDate=Carbon::now()->format('Y-m-d');
+        $revenueThisMonth=Order::where('status','!=','cancelled')
+        ->whereDate('created_at','>=',$startOfMonth)
+        ->whereDate('created_at','>=',$currentDate)
+        ->sum('grand_total');
+
+        //Last month revenue
+        $lastMonthStartDate=Carbon::now()->subMonth()->startOfMonth()->format('Y-m-d');
+        $lastMonthEndDate=Carbon::now()->subMonth()->endOfMonth()->format('Y-m-d');
+        $revenueLastMonth=Order::where('status','!=','cancelled')
+        ->whereDate('created_at','>=',$lastMonthStartDate)
+        ->whereDate('created_at','>=',$lastMonthEndDate)
+        ->sum('grand_total');
+
+        return view('admin.dashboard.dashboard', compact('breadcrumb', 'user','totalOrders','totalCustomers','totalProducts','totalRevenue','revenueLastMonth','revenueThisMonth'));
     }
 }
--- resources/views/admin/dashboard/dashboard.blade.php
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -36,7 +36,6 @@
                             <div class="d-flex align-items-end justify-content-between mt-4">
                                 <div>
                                     <h4 class="fs-22 fw-semibold ff-secondary mb-4">Rs. <span class="counter-value" data-target="{{ $totalRevenue }}">0</span></h4>
-                                    <a href="" class="text-decoration-underline">View net earnings</a>
                                 </div>
                                 <div class="avatar-sm flex-shrink-0">
                                     <span class="avatar-title bg-soft-success rounded fs-3">
@@ -47,7 +46,60 @@
                         </div><!-- end card body -->
                     </div><!-- end card -->
                 </div><!-- end col -->
-
+                <div class="col-xl-3 col-md-6">
+                    <!-- card -->
+                    <div class="card card-animate">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="flex-grow-1 overflow-hidden">
+                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0"> Revenue This Month</p>
+                                </div>
+                                <div class="flex-shrink-0">
+                                    <h5 class="text-success fs-14 mb-0">
+                                        <i class="ri-arrow-right-up-line fs-13 align-middle"></i> +16.24 %
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                <div>
+                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4">Rs. <span class="counter-value" data-target="{{ $revenueThisMonth }}">0</span></h4>
+                                </div>
+                                <div class="avatar-sm flex-shrink-0">
+                                    <span class="avatar-title bg-soft-success rounded fs-3">
+                                        <i class="bx bx-dollar-circle text-success"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div><!-- end card body -->
+                    </div><!-- end card -->
+                </div><!-- end col -->
+                <div class="col-xl-3 col-md-6">
+                    <!-- card -->
+                    <div class="card card-animate">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="flex-grow-1 overflow-hidden">
+                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0"> Revenue Last Month</p>
+                                </div>
+                                <div class="flex-shrink-0">
+                                    <h5 class="text-success fs-14 mb-0">
+                                        <i class="ri-arrow-right-up-line fs-13 align-middle"></i> +16.24 %
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                <div>
+                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4">Rs. <span class="counter-value" data-target="{{ $revenueLastMonth }}">0</span></h4>
+                                </div>
+                                <div class="avatar-sm flex-shrink-0">
+                                    <span class="avatar-title bg-soft-success rounded fs-3">
+                                        <i class="bx bx-dollar-circle text-success"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div><!-- end card body -->
+                    </div><!-- end card -->
+                </div><!-- end col -->
                 <div class="col-xl-3 col-md-6">
                     <!-- card -->
                     <div class="card card-animate">
@@ -136,70 +188,7 @@
                 </div><!-- end col -->
             </div> <!-- end row-->
 
-            <div class="row">
-                <div class="col-xl-8">
-                    <div class="card">
-                        <div class="card-header border-0 align-items-center d-flex">
-                            <h4 class="card-title mb-0 flex-grow-1">Revenue</h4>
-                            <div>
-                                <button type="button" class="btn btn-soft-secondary btn-sm">
-                                    ALL
-                                </button>
-                                <button type="button" class="btn btn-soft-secondary btn-sm">
-                                    1M
-                                </button>
-                                <button type="button" class="btn btn-soft-secondary btn-sm">
-                                    6M
-                                </button>
-                                <button type="button" class="btn btn-soft-primary btn-sm">
-                                    1Y
-                                </button>
-                            </div>
-                        </div><!-- end card header -->
 
-                        <div class="card-header p-0 border-0 bg-soft-light">
-                            <div class="row g-0 text-center">
-                                <div class="col-6 col-sm-3">
-                                    <div class="p-3 border border-dashed border-start-0">
-                                        <h5 class="mb-1"><span class="counter-value" data-target="7585">0</span></h5>
-                                        <p class="text-muted mb-0">Orders</p>
-                                    </div>
-                                </div>
-                                <!--end col-->
-                                <div class="col-6 col-sm-3">
-                                    <div class="p-3 border border-dashed border-start-0">
-                                        <h5 class="mb-1">$<span class="counter-value" data-target="22.89">0</span>k</h5>
-                                        <p class="text-muted mb-0">Earnings</p>
-                                    </div>
-                                </div>
-                                <!--end col-->
-                                <div class="col-6 col-sm-3">
-                                    <div class="p-3 border border-dashed border-start-0">
-                                        <h5 class="mb-1"><span class="counter-value" data-target="367">0</span></h5>
-                                        <p class="text-muted mb-0">Refunds</p>
-                                    </div>
-                                </div>
-                                <!--end col-->
-                                <div class="col-6 col-sm-3">
-                                    <div class="p-3 border border-dashed border-start-0 border-end-0">
-                                        <h5 class="mb-1 text-success"><span class="counter-value" data-target="18.92">0</span>%</h5>
-                                        <p class="text-muted mb-0">Conversation Ratio</p>
-                                    </div>
-                                </div>
-                                <!--end col-->
-                            </div>
-                        </div><!-- end card header -->
-
-                        <div class="card-body p-0 pb-2">
-                            <div class="w-100">
-                                <div id="customer_impression_charts" data-colors='["--vz-primary", "--vz-success", "--vz-danger"]' class="apex-charts" dir="ltr"></div>
-                            </div>
-                        </div><!-- end card body -->
-                    </div><!-- end card -->
-                </div><!-- end col -->
-
-
-            </div>
 
             <div class="row">
                 <div class="col-xl-6">
@@ -382,63 +371,6 @@
             <div class="row">
 
 
-                <div class="col-xl-8">
-                    <div class="card">
-                        <div class="card-header align-items-center d-flex">
-                            <h4 class="card-title mb-0 flex-grow-1">Recent Orders</h4>
-                            <div class="flex-shrink-0">
-                                <button type="button" class="btn btn-soft-info btn-sm">
-                                    <i class="ri-file-list-3-line align-middle"></i> Generate Report
-                                </button>
-                            </div>
-                        </div><!-- end card header -->
-
-                        <div class="card-body">
-                            <div class="table-responsive table-card">
-                                <table class="table table-borderless table-centered align-middle table-nowrap mb-0">
-                                    <thead class="text-muted table-light">
-                                        <tr>
-                                            <th scope="col">Order ID</th>
-                                            <th scope="col">Customer</th>
-                                            <th scope="col">Product</th>
-                                            <th scope="col">Amount</th>
-                                            <th scope="col">Vendor</th>
-                                            <th scope="col">Status</th>
-                                            <th scope="col">Rating</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <a href="apps-ecommerce-order-details.html" class="fw-medium link-primary">#VZ2112</a>
-                                            </td>
-                                            <td>
-                                                <div class="d-flex align-items-center">
-                                                    <div class="flex-shrink-0 me-2">
-                                                        <img src="assets/images/users/avatar-1.jpg" alt="" class="avatar-xs rounded-circle" />
-                                                    </div>
-                                                    <div class="flex-grow-1">Alex Smith</div>
-                                                </div>
-                                            </td>
-                                            <td>Clothes</td>
-                                            <td>
-                                                <span class="text-success">$109.00</span>
-                                            </td>
-                                            <td>Zoetic Fashion</td>
-                                            <td>
-                                                <span class="badge badge-soft-success">Paid</span>
-                                            </td>
-                                            <td>
-                                                <h5 class="fs-14 fw-medium mb-0">5.0<span class="text-muted fs-11 ms-1">(61 votes)</span></h5>
-                                            </td>
-                                        </tr><!-- end tr -->
-
-                                    </tbody><!-- end tbody -->
-                                </table><!-- end table -->
-                            </div>
-                        </div>
-                    </div> <!-- .card-->
-                </div> <!-- .col-->
             </div> <!-- end row-->
 
         </div> <!-- end .h-100-->
